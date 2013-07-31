30993373326297d84bebe15e6603bf57dec95d4b
Aditya Mukherjee <aditya@adityamukherjee.com>
2013-07-31 12:25:46 -0400

    added some footnotes post processing

--- idea/idea.js
+++ idea/idea.js
@@ -1,6 +1,32 @@
-// ligaturise everything (kinda)
-$.fn.ligature = function(str, lig) {
-    return this.each(function() {
-        this.innerHTML = this.innerHTML.replace(new RegExp(str, 'g'), lig);
-    });
-};
+function doFootnotes(){
+	$(".footnotes li").each(function(){
+		var fnLink = $(this).find("a[rev=footnote]");
+		var relTo = $(this).parents(".post").find(".related-to .fr");
+
+		var href = fnLink.attr("href");
+			href = href.replace(/\:/g, "\\:");
+		var supStr = "sup"+href;
+		var fnLoc = $(this).parents(".text").find(supStr);
+			fnLocText = fnLoc.text();
+
+		fnLink.remove();
+		var fnText = $( "<div>"+ $(this).html() +"</div>").find("p:eq(0)").prepend( fnLocText+". " )
+
+		console.log(fnText);
+
+		relTo.before(
+			$("<div class='fn'></div>")
+				.html( fnText )
+				//.css("top", fnLoc.position().top+"px")
+		)
+	});
+	$("a[rel=footnote]").each(function(){
+		var fn = $(this).text();
+		$(this).before(fn);
+		$(this).remove();
+	});
+}
+
+$(document).ready(function(){
+	doFootnotes();
+});
